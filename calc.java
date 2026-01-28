1028d042a0b8bb8ed0bd88af48d608e83698797c
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-01-28 21:00:31 +0530

    first commit

---
+++ calc.java
@@ -0,0 +1,13 @@
+public class calc {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum= a+b;
+        int diff= b-a;
+        int mul=a*b;
+        System.out.println("Sum:"+sum);
+        System.out.println("Diff:"+diff);
+        System.out.println("mul:"+mul);
+    
+    }
+}
