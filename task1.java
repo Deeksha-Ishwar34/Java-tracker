7e1a1c0001929eff0989b2f962ac5d613d765443
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-02-04 20:23:30 +0530

    first commit

---
+++ task1.java
@@ -0,0 +1,11 @@
+public class task1 {
+    public static void main(String[] args) {
+        int num=7;
+        if(num>10){
+            System.out.println("The number is greater than 10");
+        }
+        else{
+            System.out.println("The number is not greater than 10");
+        }
+    }
+}
