34c9ad81a6711dd5282384813c392ef1b1623cf2
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-02-06 20:44:29 +0530

    first commit

---
+++ task2.java
@@ -0,0 +1,12 @@
+public class task2 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(a==b){
+           System.out.println("The numbers are equal");
+        }
+        else{
+            System.out.println("The numbers are not equal");
+        }
+    }
+}
