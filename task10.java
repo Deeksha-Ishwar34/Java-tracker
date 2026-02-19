2aac9654a6edae211ed97a6ed1663305d3863449
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-02-19 21:34:18 +0530

    first commit

---
+++ task10.java
@@ -0,0 +1,15 @@
+public class task10 {
+    public static void main(String[] args){
+        int n=9;
+        for(int i=1;i<=n;i++){
+            int j=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+        
+            System.out.println();
+            
+        }
+    }
+}
