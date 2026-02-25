a2d05719562fa61b1e50d63a727e813735cb23f5
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-02-25 22:33:39 +0530

    first commit

---
+++ task45.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class task45 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=getNum(sc);
+        sc.nextLine();
+        getTasks(num,sc);
+        sc.close();
+     }
+     public static int getNum(Scanner sc){
+      System.out.println("How many tasks? ");
+      return sc.nextInt();
+     }
+     public static void getTasks(int num,Scanner sc){
+      ArrayList<String> tasks=new ArrayList<>();
+      for(int i=0;i<num;i++){
+         System.out.println("Enter task: ");
+         tasks.add(sc.nextLine());
+      }
+     
+     }
+}
+
