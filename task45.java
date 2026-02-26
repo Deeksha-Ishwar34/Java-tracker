821cf009541177b72b04ebd735cc560cf5430c1d
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-02-26 22:25:36 +0530

    first commit

--- task45.java
+++ task45.java
@@ -1,25 +1,31 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class task45 {
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num=getNum(sc);
-        sc.nextLine();
-        getTasks(num,sc);
-        sc.close();
-     }
-     public static int getNum(Scanner sc){
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int num = getNum(sc);
+      sc.nextLine();
+      getTasks(num, sc);
+      sc.close();
+   }
+
+   public static int getNum(Scanner sc) {
       System.out.println("How many tasks? ");
       return sc.nextInt();
-     }
-     public static void getTasks(int num,Scanner sc){
-      ArrayList<String> tasks=new ArrayList<>();
-      for(int i=0;i<num;i++){
+   }
+
+   public static void getTasks(int num, Scanner sc) {
+      ArrayList<String> tasks = new ArrayList<>();
+      for (int i = 0; i < num; i++) {
          System.out.println("Enter task: ");
          tasks.add(sc.nextLine());
-      }
-     System.out.println("---TO DO LIST---");
-     System.out.println("");
-     }
-}
 
+      }
+      System.out.println("---TO DO LIST---");
+      for (int i = 0; i < num; i++) {
+         System.out.println(i + " --> " + tasks.get(i));
+      }
+
+   }
+}
