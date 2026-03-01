c65c5cb86a2290c77ebcf87e0e6ae2cd23130ad0
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-01 21:40:19 +0530

    first commit

--- task15.java
+++ task15.java
@@ -1,9 +1,9 @@
 public class task15 {
     public static void main(String[] args) {
-       int i=10;
-       while(i>=1){
+       int i=0;
+       while(i<=10){
         System.out.println(i);
-        i--;
+        i++;
        }
     }
 }
