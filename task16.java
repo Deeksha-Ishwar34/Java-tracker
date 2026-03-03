0f5c964dc00757932c706609b3680d5bdcb74924
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-03 22:11:18 +0530

    first commit

---
+++ task16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class task16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num;
+       do{
+            System.out.println("Enter a number greater than 0:- ");
+             num=sc.nextInt();
+       } while(num<0);
+       sc.close();
+        
+
+
+    }
+}
