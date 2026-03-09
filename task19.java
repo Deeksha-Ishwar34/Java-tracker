60264092484d72f428abf9bfbe616d19a68ad286
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-09 20:57:01 +0530

    first commit

---
+++ task19.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class task19 {
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int num;
+            do{
+                System.out.println("Enter a positive number:- ");
+                num=sc.nextInt();
+            }while(num<=0);
+            System.out.println("Valid number entered is"+num);
+            sc.close();
+        }
+    }
