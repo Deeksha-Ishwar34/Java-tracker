7b41996381c832984b6c21a1552cdc3b591089f6
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-17 19:32:14 +0530

    first commit

---
+++ task54.java
@@ -0,0 +1,22 @@
+public class task54 {
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 8, 1, 9, 2 };
+        int first_largest = arr[0];
+        int sec_largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > first_largest) {
+                first_largest = arr[i];
+            }
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == first_largest) {
+                continue;
+            } else if (sec_largest < arr[i]) {
+                sec_largest = arr[i];
+            }
+        }
+        System.out.println(sec_largest);
+
+    }
+
+}
