2eee84ddc7a4be5384e761908b92cd4136886018
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-20 14:33:54 +0530

    first commit

--- task66.java
+++ task66.java
@@ -1,17 +1,23 @@
 public class task66 {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6};
-        calcSum_Avg(arr);  
-    }
-    public static void calcSum_Avg(int[] arr){
-        int sum=0;
-        double avg=0;
-        for(int i=0;i<arr.length;i++){
-            sum=sum+arr[i];
+        int[] arr = { 1, 2, 3, 4, 5 };
+        if (arr.length == 0) {
+            System.out.println("Array is empty");
+        } else {
+            calcSum_Avg(arr);
         }
-         avg= (double) sum/arr.length;
-         System.out.println("Sum = "+sum);
-          System.out.println("Average = "+avg);
+
     }
-      
+
+    public static void calcSum_Avg(int[] arr) {
+        int sum = 0;
+        double avg = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        avg = (double) sum / arr.length;
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + avg);
+    }
+
 }
