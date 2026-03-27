06d89b5adb946dd1da379c3460d9ffd33349b961
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-27 21:29:05 +0530

    first commit

--- task68.java
+++ task68.java
@@ -1,6 +1,6 @@
 public class task68 {
     public static void main(String[] args) {
-        int[] arr= {1, 1, 3, 1, 5, 5};
+        int[] arr= {1, 2, 3, 4, 6, 5};
         sum(arr);
     }
     public static void sum(int[] arr){
