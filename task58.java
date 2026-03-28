01e4ace3ac700958d510d65be398f9de84711495
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-28 15:21:11 +0530

    first commit

---
+++ task58.java
@@ -0,0 +1,18 @@
+public class task58 {
+    public static void main(String[] args) {
+        int[] arr={1,3,4,6,1,3,4,9,6,6};
+        freq(arr);
+    }
+    public static void freq(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+             System.out.printf("%d appears %d times\n", arr[i], count);
+        }
+        
+    }
+}
