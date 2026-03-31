7ed8f69f90506d4ace47122f3772c989bcef6371
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-03-31 21:54:42 +0530

    first commit

--- task70.java
+++ task70.java
@@ -1,3 +1,24 @@
 public class task70 {
+    public static void main(String[] args) {
+        int[] arr={2,7,11,15};
+        int target=9;
+        twosum(arr,target);
+    }
+    public static void twosum(int[] arr,int target){
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+            if(arr[left]+arr[right]==target){
+                System.out.printf("%d %d",left,right);
+                return;
+            }
+            else if(arr[left]+arr[right]>target){
+                right--;
+            }
+            else if(arr[left]+arr[right]<target){
+                left++;
+            }
+        }
+    }
     
 }
