a2d85ab238ff923a08e3bd881a30a29c1ed435f8
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-04-01 22:28:01 +0530

    first commit

--- task71.java
+++ task71.java
@@ -1,3 +1,10 @@
+import java.util.HashMap;
 public class task71 {
-    
+    public static void main(String[] args) {
+        int[] arr={1, 2, 1, 3, 2, 1};
+        HashMap<String, Integer> marks = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            
+        }
+    }
 }
