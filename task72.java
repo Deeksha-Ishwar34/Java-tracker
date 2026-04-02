18d500b51b56c7315b3084af46fc6c27ec31222b
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-04-02 19:13:34 +0530

    first commit

--- task72.java
+++ task72.java
@@ -1,3 +1,18 @@
+import java.util.HashMap;
 public class task72 {
-    
+    public static void main(String[] args) {
+        int[] arr={8,3,6,8,7};
+         HashMap<Integer, Integer> map = new HashMap<>();
+         int target=10;
+         for(int i=0;i<arr.length;i++){
+            map.put(arr[i],i);
+            int needed=target-arr[i];
+            if(map.containsKey(needed)){
+                System.out.println(map.get(arr[i]));
+                System.out.println(map.get(needed));
+                return;
+            }
+         }
+
+        }
 }
