fd3538a6e0a309b118ba2f751a9d2a83ea1d921a
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-04-10 10:49:33 +0530

    first commit

--- task76.java
+++ task76.java
@@ -1,5 +1,18 @@
 public class task76 {
     public static void main(String[] args) {
-        
+        int[] arr={1,2,3,4,5};
+        reverse(arr);
+    }
+    public static void reverse(int[] arr){
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+           arr[left]=arr[right];
+           left++;
+           right--;
+        }
+        for(int num:arr){
+            System.out.println(num+" ");
+        }
     }
 }
