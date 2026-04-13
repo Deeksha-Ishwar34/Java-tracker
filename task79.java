d31fef33765fb78b46811cadc5b08e424c1756bf
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-04-13 20:27:24 +0530

    first commit

--- task79.java
+++ task79.java
@@ -1,5 +1,21 @@
 public class task79 {
     public static void main(String[] args) {
-        
+        int[] arr = { 2, 1, 0, 0, 4, 12 };
+        move(arr);
+    }
+
+    public static void move(int[] arr) {
+        int slow = 0;
+        for (int fast = 0; fast < arr.length; fast++) {
+            if (arr[fast] != 0) {
+                int temp = arr[slow];
+                arr[slow] = arr[fast];
+                arr[fast] = temp;
+                slow++;
+            }
+        }
+        for (int num : arr) {
+                System.out.println(num + "");
+            }
     }
 }
