1c26a2a949ab29c5bb98b747d82865db02ea3584
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-04-14 17:25:04 +0530

    first commit

---
+++ task30.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class task30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int units=getUnits(sc);
+        int bill=calcbill(units);
+        System.out.println("Total bill: "+bill);
+    }
+    public static int getUnits(Scanner sc) {
+        System.out.println("Enter Units: ");
+        return sc.nextInt();
+    }
+    public static int calcbill(int units)
+{
+    if (units < 0) {
+        return -1;
+    }
+    else if (units <= 100) {
+        return units * 1;
+    }
+    else if (units <= 200) {
+        return (100 * 1) + ((units - 100) * 2);
+    }
+    else {
+        return (100 * 1) + (100 * 2) + ((units - 200) * 3);
+    }
+}
+
+        
+    }
+
