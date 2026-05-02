4c89cc3b6b3f839bf306aa03793cb0bfc3662f38
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-02 17:10:04 +0530

    first commit

--- task90.java
+++ task90.java
@@ -1,3 +1,23 @@
+import java.util.*;
 public class task90 {
-    
+    public static void main(String[] args) {
+        int[] arr={7, -3,1, 4, 8};
+        Sorted(arr);
+    }
+    public static void Sorted(int[] arr){
+        int res[]=new int[arr.length];
+        int l=0;
+        int r=arr.length-1;
+        for(int indx=arr.length-1;indx>=0;indx--){
+             if(Math.abs(arr[r])>Math.abs(arr[l])){
+            res[indx]=Math.abs(arr[r]);
+            r--;
+        }
+        else{
+            res[indx]=Math.abs(arr[l]);
+            l++;
+        }
+        }
+       System.out.println(Arrays.toString(res));
+    }
 }
