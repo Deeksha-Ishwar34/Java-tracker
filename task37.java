19b7f7a08b037f12791995e24aa4ba21d23df81f
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-04 21:41:34 +0530

    first commit

---
+++ task37.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class task37 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = getN(sc);
+      String[] colors = getColors(n, sc);
+      display(colors, n);
+      sc.close();
+   }
+
+   public static int getN(Scanner sc) {
+      System.out.println("How many colors do u want to enter? ");
+      return sc.nextInt();
+
+   }
+
+   public static String[] getColors(int n, Scanner sc) {
+      String[] colors = new String[n];
+      sc.nextLine();
+      for (int i = 0; i < n; i++) {
+         System.out.printf("Enter Color %d", (i + 1));
+         colors[i] = sc.nextLine();
+      }
+      return colors;
+   }
+
+   public static void display(String[] colors, int n) {
+      System.out.println("Colors u entered");
+      for (int i = 0; i < n; i++) {
+         System.out.println(colors[i]);
+      }
+   }
+}
