9c83d2a864ceefee539e2cc60754a02c302cf95a
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-10 20:21:06 +0530

    first commit

--- task39.java
+++ task39.java
@@ -1,1 +1,41 @@
+import java.util.Scanner;
+public class task39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = getSize(sc);
+        int[] nums = getNums(sc, size);
+        searching(nums);
+        sc.close();
+    }
+
+    public static int getSize(Scanner sc) {
+        System.out.println("Enter size: ");
+        return sc.nextInt();
+    }
+
+    public static int[] getNums(Scanner sc, int size) {
+        int[] nums = new int[size];
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("Enter element " + (i + 1));
+            nums[i] = sc.nextInt();
+        }
+        return nums;
+    }
+
+    public static void searching(int[] nums){
+        boolean found=false;
+        int search=88;
+        for(int num:nums){
+            if(num==search){
+                found=true;
+            }
+        }
+        if(found){
+            System.out.println("element found");
+        }
+        else{
+            System.out.println("element not found");
+        }
+     }
+}
 
