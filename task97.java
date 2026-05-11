dce2d13d06ee57e3b6dd26180bc5dff837351593
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-11 15:45:08 +0530

    first commit

--- task97.java
+++ task97.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class task97 {
     public static void main(String[] args) {
-        
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:- ");
+        String s=sc.nextLine();
+        StringBuilder sb=new StringBuilder(s);
+        System.out.println("after reversing your name:- "+sb.reverse());
+        sc.close();
     }
 }
