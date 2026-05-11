0d9a3be38b7ed50e6e60a7ffab674f0605a0fd0d
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-11 15:09:44 +0530

    first commit

--- task95.java
+++ task95.java
@@ -1,8 +1,7 @@
 public class task95 {
     public static void main(String[] args) {
         int a=3;
-        int b=5;
-        
+        int b=5;  
         System.out.println("Sum:- "+(a+b));
     }
 }
