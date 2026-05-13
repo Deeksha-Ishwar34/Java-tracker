f4213ceeceb3e29cab687fa7651a9b43dcfca9d3
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-13 12:53:30 +0530

    first commit

--- task98.java
+++ task98.java
@@ -1,5 +1,10 @@
 public class task98 {
     public static void main(String[] args) {
-        
+       int curyear=2026;
+       int byear=2006;
+       int age=curyear-byear;
+       int ageInMonths=age*12;
+       int ageInDays=age*365;
+       System.out.printf("age:- %d,age in months:- %d,age in days:- %d",age,ageInMonths,ageInDays); 
     }
 }
