fa277c03523897dc4887bc6b2b57eda237f47040
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-14 11:03:05 +0530

    first commit

--- task99.java
+++ task99.java
@@ -1,5 +1,18 @@
 public class task99 {
    public static void main(String[] args) {
-    
+     String s="A man, a plan, a canal: Panama";
+    s = s.replaceAll("[^A-Za-z0-9]", "");
+    s=s.toLowerCase();
+     int l=0;
+     int r=s.length()-1;
+     while (l<r) {
+       if(s.charAt(l)!=s.charAt(r)){
+        System.out.println("not a palindrome");
+        return;
+       }
+       l++;
+       r--;
    } 
+   System.out.println("Palindrome");
+}
 }
