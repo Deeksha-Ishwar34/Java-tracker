30bd13f02683657835909ea30ccb7498be8d2146
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-16 11:55:23 +0530

    first commit

--- task37.java
+++ task37.java
@@ -25,7 +25,7 @@ public class task37 {
    }
 
    public static void display(String[] colors, int n) {
-      System.out.println("Colors u entered");
+      System.out.println("Colors u entered ");
       for (int i = 0; i < n; i++) {
          System.out.println(colors[i]);
       }
