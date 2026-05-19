93bb091dada0f1ac5e6653cf7d019355efa945f0
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-19 22:21:33 +0530

    first commit

---
+++ task41.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class task41 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+
+        int n = getSize(sc);
+        sc.nextLine();  
+
+        getNames(n, sc, names);
+        displayNames(names);
+        findLongestName(names);
+
+        sc.close();
+    }
+
+    public static int getSize(Scanner sc){
+        System.out.print("How many names? ");
+        return sc.nextInt();
+    }
+
+    public static void getNames(int n, Scanner sc, ArrayList<String> names){
+        for(int i = 0; i < n; i++){
+            System.out.print("Enter name " + (i+1) + ": ");
+            String name = sc.nextLine();
+            names.add(name);
+        }
+    }
+
+    public static void displayNames(ArrayList<String> names){
+        System.out.println("\nNames: " + names);
+        System.out.println("Total names: " + names.size());
+    }
+
+    public static void findLongestName(ArrayList<String> names){
+        String longest = names.get(0);
+
+        for(String name : names){
+            if(name.length() > longest.length()){
+                longest = name;
+            }
+        }
+
+        System.out.println("Longest name: " + longest);
+    }
+}
