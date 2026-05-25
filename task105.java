21a6d799481939daee80687af27f44022854c5b4
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-05-25 23:03:41 +0530

    first commit

--- task105.java
+++ task105.java
@@ -1,5 +1,23 @@
 public class task105 {
     public static void main(String[] args) {
-        
+        String s="abciiidef";
+        int k=3;
+        max(s,k);
+    }
+    public static void max(String s,int k){
+        int maxsum=0;
+        int winsum=0;
+        for(int i=0;i<k;i++){
+            if(isVowel(s.charAt(i))){
+                winsum++;
+            }
+        }
+       
+    }
+    public static boolean isVowel(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            return true;
+        }
+        return false;
     }
 }
