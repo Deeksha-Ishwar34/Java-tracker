e3a5346639a122c7783abdb6132e299d4053dc6d
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-06-17 11:54:48 +0530

    first commit

--- task112.java
+++ task112.java
@@ -1,3 +1,25 @@
 public class task112 {
-    
+    public static void main(String[] args) {
+        int[] arr={0,1,1,1,1,1,0,0,0};
+        longContiArr(arr);
+    }
+    public static void longContiArr(int[] arr){
+        int maxcount=0;
+        for(int i=0;i<arr.length;i++){
+            int zerocount=0;
+            int onecount=0;
+            for(int j=i;j<arr.length;j++){
+                if(arr[i]==0){
+                    zerocount++;
+                }
+                else{
+                    onecount++;
+                }
+                if (zerocount==onecount){
+                    maxcount=Math.max(maxcount,j-i+1);
+                }
+            }
+        }
+        System.out.println(maxcount);
+    }
 }
