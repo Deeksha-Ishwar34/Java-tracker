b9685d8c9f743e643de754122823b14ecce6abf2
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-06-18 10:20:50 +0530

    first commit

--- task113.java
+++ task113.java
@@ -1,5 +1,19 @@
 public class task113 {
     public static void main(String[] args) {
-        
+          int[] arr={23,2,4,6,7};
+          int k=6;
+          System.out.println(SubArrSum(arr,k));
     }
+     public static boolean SubArrSum(int[] arr,int k){
+         for(int i=0;i<arr.length-1;i++){
+            int sum=0;
+             for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum % k==0 && j-i+1>=2){
+                    return true;
+                }
+             }
+         }
+        return false;
+}
 }
