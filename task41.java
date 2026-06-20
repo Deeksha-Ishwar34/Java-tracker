58f5ba1b624551fbcd318fbfd6fc3deec5e7a0bc
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-06-20 18:18:34 +0530

    first commit

--- task41.java
+++ task41.java
@@ -24,7 +24,7 @@ public class task41 {
     }
     public static void displayNames(ArrayList<String> names){
         System.out.println("\n Names: " + names);
-        System.out.println("Total names: " + names.size());
+        System.out.println("Total names:- " + names.size());
     }
     public static void findLongestName(ArrayList<String> names){
         String longest = names.get(0);
