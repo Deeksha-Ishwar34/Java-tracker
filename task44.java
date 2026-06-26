b0ec6c4fdbf421d428f4ac8cad295b5aa32dc719
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-06-26 20:04:40 +0530

    first commit

--- task44.java
+++ task44.java
@@ -5,7 +5,7 @@ public class task44 {
         Scanner sc = new Scanner(System.in);
         HashMap<String,Integer> marks=new HashMap<>();
         marks.put("Deeksha",99);
-        marks.put("Ria ",55);
+        marks.put("Ria",55);
         marks.put("Evita",65);
         marks.put("Alaina",73);
         System.out.println("Student's names List");
