6ae1d257a4a2f007c925a613fe68b3a70afe5c78
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-06-28 16:04:15 +0530

    first commit

--- task46.java
+++ task46.java
@@ -1,3 +1,20 @@
 public class task46 {
-    
+     public static void main(String[] args) {
+        mobile m1=new mobile("Vivo",12000);
+         mobile m2=new mobile("Oppo",15000);
+         m1.display();
+         m2.display();
+     }   
 }
+class mobile{
+    String brand;
+    int price;
+    mobile(String brand,int price){
+        this.brand=brand;
+        this.price=price;
+    }
+    void display(){
+        System.out.println("Mobile brand "+brand+"price "+price);
+    }
+}
+
