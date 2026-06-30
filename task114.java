b000eb2f7ea3084330afd591c1b89ce28b7950bb
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-06-30 12:47:11 +0530

    first commit

--- task114.java
+++ task114.java
@@ -1,3 +1,19 @@
+import java.util.Arrays;
 public class task114 {
-    
+    public static void main(String[] args) {
+        int[] arr={0,1,0,3,12};
+        movezeros(arr);
+    }
+    public static void movezeros(int[] arr){
+        int slow=0;
+        for(int fast=0;fast<arr.length;fast++){
+            if(arr[fast]!=0){
+                int temp=arr[slow];
+                arr[slow]=arr[fast];
+                arr[fast]=temp;
+                slow++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
