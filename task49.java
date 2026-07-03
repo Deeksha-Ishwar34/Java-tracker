bc78cbdc28f47a0f1910895e34820027c172871b
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-03 21:08:45 +0530

    first commit

--- task49.java
+++ task49.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class task49 {
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number: ");
         int num1=sc.nextInt();
         System.out.println("Enter second number: ");
