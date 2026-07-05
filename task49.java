fb966677f63b6076d2c897ac7ef750aab039c11f
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-05 16:46:55 +0530

    first commit

--- task49.java
+++ task49.java
@@ -3,7 +3,7 @@ public class task49 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number:- ");
-        int num1=sc.nextInt();
+        int num1 =sc.nextInt();
         System.out.println("Enter second number:- ");
         int num2=sc.nextInt();
         int ans=num1+num2;
