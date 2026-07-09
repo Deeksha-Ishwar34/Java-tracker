38dbe8c33fafe0f1948988a1263cac1c9f2ee97c
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-09 21:57:30 +0530

    first commit

--- task51.java
+++ task51.java
@@ -1,8 +1,8 @@
 public class task51 {
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i = 1;i <= 10; i++){
-            sum= sum+i;
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum=sum+i;
         }
         System.out.println(sum);
     }
