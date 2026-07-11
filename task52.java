ca1886a142e48c5c71653168449812f6f25d4694
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-11 21:49:53 +0530

    first commit

--- task52.java
+++ task52.java
@@ -1,6 +1,6 @@
 public class task52 {
     public static void main(String[] args) {
-        int[] arr={44,36,5,76,58};
+        int[] arr={44,36,59,76,58};
          int target=arr[0];
         for(int i=1;i<arr.length;i++){
             if(arr[i]>target){
