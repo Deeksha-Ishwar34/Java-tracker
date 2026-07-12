0571386fc0efd446cdb91f51d2a781bf3227593b
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-12 20:25:25 +0530

    first commit

--- task53.java
+++ task53.java
@@ -1,3 +1,8 @@
 public class task53 {
-    
+    public static void main(String[] args) {
+        int[] arr={4,6,5,87,9};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
 }
