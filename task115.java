f7d6b427cefd9244b0bbb2e372d08755b0fd3327
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-13 21:19:27 +0530

    first commit

--- task115.java
+++ task115.java
@@ -1,6 +1,23 @@
+import java.util.HashMap;
 public class task115 {
     public static void main(String[] args) {
-        int arr[]={2, 3, 5, 4, 5, 3, 4};
+        int[] arr={2, 3, 5, 4, 5, 3};
         findOddOne(arr);
+    }
+    public static void findOddOne(int[] arr){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                 map.put(arr[i],1);
+            }
+        }
+       for(int num:map.keySet()){
+        if(map.get(num)==1){
+            System.out.println(num);
+        }
+       }
     }
 }
