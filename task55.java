3092b5ea7de63cb22e38f6b9c49d838d8d6f3a0b
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-16 20:57:00 +0530

    first commit

--- task55.java
+++ task55.java
@@ -1,6 +1,6 @@
 public class task55 {
     public static void main(String[] args) {
-        int[] arr={5,1,8,2,6};
+        int[] arr={51,15,8,2,6};
         int first=Integer.MIN_VALUE;
         int second=Integer.MIN_VALUE;
         for(int num:arr){
