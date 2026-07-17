441c8273868c540d9a9437a6bad8cb9acfc2b31a
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-17 21:30:22 +0530

    first commit

--- task56.java
+++ task56.java
@@ -1,5 +1,17 @@
 public class task56 {
     public static void main(String[] args) {
-        
+        int[] arr={3,5,8,6,4,9};
+        int small=arr[0];
+        int large=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>large){
+                large=arr[i];
+            }
+            else if (arr[i]<small){
+                small=arr[i];
+            }
+        }
+        System.out.println(small);
+        System.out.println(large);
     }
 }
