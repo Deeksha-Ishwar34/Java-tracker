b58135e8efa28c3a6192c5e210e86366f4f4d9a9
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-17 21:31:11 +0530

    first commit

--- task56.java
+++ task56.java
@@ -1,6 +1,6 @@
 public class task56 {
     public static void main(String[] args) {
-        int[] arr={3,5,8,6,4,9};
+        int[] arr={31,5,8,6,4,9};
         int small=arr[0];
         int large=arr[0];
         for(int i=0;i<arr.length;i++){
