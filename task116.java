4f6efe96087c4eba9c01df62bb6daed61e18919b
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-20 12:24:25 +0530

    first commit

--- task116.java
+++ task116.java
@@ -1,3 +1,17 @@
 public class task116 {
-    
+    public static void main(String[] args) {
+        int[] arr={2,7,11,15};
+        int target=9;
+        twosum(arr,target);
+    }
+    public static void twosum(int[] arr,int target){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length-1;j++){
+            int needed=target-arr[i];
+            if(arr[j]==needed){
+                System.out.printf("[%d,%d]",i,j);
+            }
+        }
+        }
+    }
 }
