7e7fd2924dfda949f0ec475a7895a835187ffa56
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-21 22:06:41 +0530

    first commit

--- task118.java
+++ task118.java
@@ -1,13 +1,20 @@
+import java.util.Arrays;
 public class task118 {
     public static void main(String[] args) {
-        int[] arr={1,2,2,1,3};
+        int[] arr={1,2,2,3,3,4};
         removedupli(arr);
     }
     public static void removedupli(int[] arr){
         for(int i=0;i<arr.length-1;i++){
            for(int j=i+1;j<arr.length;j++){
-            
+            if(arr[i]!=arr[j]){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
         } 
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
