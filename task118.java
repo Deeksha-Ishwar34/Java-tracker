6fec13453d418716c26f3ea04174ee1f5e929791
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-21 18:19:05 +0530

    first commit

--- task118.java
+++ task118.java
@@ -1,3 +1,13 @@
 public class task118 {
-    
+    public static void main(String[] args) {
+        int[] arr={1,2,2,1,3};
+        removedupli(arr);
+    }
+    public static void removedupli(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+           for(int j=i+1;j<arr.length;j++){
+            
+        } 
+        }
+    }
 }
