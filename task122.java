05f1440fabb8c820f51a3409a55047dd96443f5b
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-07-26 11:35:20 +0530

    first commit

--- task122.java
+++ task122.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class task122 {
-    
+    public static void main(String[] args) {
+         int[] arr={0,1,2,2,3,0,4,2};
+         int val=2;
+        removeval(arr,val);
+    }
+    public static void removeval(int[] arr,int val){   
+      int[] temp=new int[arr.length];
+      int j=0;
+      for(int i=0;i<arr.length;i++){
+        if(arr[i]!=val){
+            temp[j]=arr[i];
+            j++;
+        }
+      }
+       System.out.println(Arrays.toString(temp));
+    }
 }
