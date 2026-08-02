510742a59c7e82b5ff80a92b8fec29a94a59203c
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-02 21:15:09 +0530

    first commit

--- task60.java
+++ task60.java
@@ -1,3 +1,10 @@
 public class task60 {
-    
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            for(int j=i;j<=2*i-1;j++){
+                 System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
