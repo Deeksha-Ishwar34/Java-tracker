2f19fd05153c95ca045698af7515c81e15df7d72
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-02 21:11:45 +0530

    first commit

---
+++ task59.java
@@ -0,0 +1,10 @@
+public class task59 {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            for(int j=i;j>=1;j--){
+                 System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
