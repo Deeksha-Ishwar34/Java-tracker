c512541a04a61fb950786be8813902dc7af6e6c8
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-03 12:04:31 +0530

    first commit

--- task125.java
+++ task125.java
@@ -1,3 +1,13 @@
 public class task125 {
-    
+    public static void main(String[] args) {
+         int[] arr={1,2,3,4,6,8};
+         missingnum(arr);
+    }
+    public static void missingnum(int[] arr){ 
+         for(int i=0;i<arr.length-1;i++){
+            if(arr[i]+1!=arr[i+1]){
+                System.out.println(arr[i]+1);
+            }
+         }
+    }
 }
