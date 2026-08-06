2a1aa0619eb85b9e2db318db5682ef1dc35b8535
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-06 12:43:55 +0530

    first commit

--- task62.java
+++ task62.java
@@ -1,29 +1,24 @@
 public class task62 {
     public static void main(String[] args) {
-        int[] arr = {1,4,5,6};
-        if(arr.length==0){
+        int[] arr = { 1, 21, 5, 6 };
+        if (arr.length == 0) {
             System.out.println("Array is empty");
-        }
-        else if(arr.length==1){
+        } else if (arr.length == 1) {
             System.out.println("array is sorted");
+        } else {
+            checking(arr);
         }
-        else{
-             checking(arr);
-        }
-       
+
     }
-    public static void checking(int[] arr){
-        boolean sorted=false;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]<=arr[i+1]){
-                sorted=true;
-        }
-            else{
-                sorted=false;
-                break;
+
+    public static void checking(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] >= arr[i + 1]) {
+                System.out.println(false);
+                return;
             }
         }
-         System.out.println(sorted);
-       
+        System.out.println(true);
+
     }
 }
