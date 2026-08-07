bdb7d7702d82909a1497975ae99ab206a79d30ad
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-07 22:06:42 +0530

    first commit

--- task63.java
+++ task63.java
@@ -1,3 +1,21 @@
 public class task63 {
-    
+    public static void main(String[] args) {
+        int[] arr={5,6,3,8,1,9};
+         int target=9;
+        if(arr.length==0){
+            System.out.println("Array is empty");
+        }
+        else{
+            int result= checking(arr,target);
+            System.out.println(result);
+        }   
+    }
+    public static int checking(int[] arr,int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
