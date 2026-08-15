b89707878d87329b8c7c5e2e4aaab9377fbf7fab
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-15 23:02:25 +0530

    first commit

--- task67.java
+++ task67.java
@@ -1,6 +1,6 @@
 public class task67 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 3, 1, 3, 3 };
+        int[] arr = {1,2,2,3,1,3,3 };
         if (arr.length == 0) {
             System.out.println("Array is empty");
             return;
