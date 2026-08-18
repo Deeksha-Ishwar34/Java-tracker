43bf0c73af56bf175c209636701a6b023c8a2586
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-18 21:15:09 +0530

    first commit

--- task67.java
+++ task67.java
@@ -11,7 +11,7 @@ public class task67 {
     public static void calcFreq(int[] arr) {
         for (int i=0;i<arr.length;i++) {
             int count=0;
-            for (int j = 0; j < arr.length; j++) {
+            for (int j=0;j<arr.length;j++) {
                 if (arr[i] == arr[j]) {
                     count++;
                 }
