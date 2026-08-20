f57bc87b2301a41abf27a14c36b10b684ab44902
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-20 11:54:46 +0530

    first commit

--- task130.java
+++ task130.java
@@ -1,3 +1,23 @@
 public class task130 {
-    
+    public static void main(String[] args) {
+         int[] arr={2,2,2,2,5,5,5,8};
+         int k=3;
+         int t=4;
+        numsub(arr,k,t);
+    }
+     public static void numsub(int[] arr,int k,int t){
+        int count=0;
+        for(int i=0;i<=arr.length-k;i++){
+             int sum=0;
+              for(int j=i;j<i+k;j++){
+                sum+=arr[j];
+              }
+                double avg=(double)sum/k;
+              if(avg>=t){
+                    count++;
+                }
+        }
+         System.out.println(count);
+       
+     }
 }
