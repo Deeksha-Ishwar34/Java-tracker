8f9ffc4e4428ef2d7f49ab718f9fc98ad7bc4371
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-24 21:49:32 +0530

    first commit

--- task71.java
+++ task71.java
@@ -2,9 +2,10 @@ import java.util.HashMap;
 public class task71 {
     public static void main(String[] args) {
         int[] arr={1, 2, 1, 3, 2, 1};
-        HashMap<String, Integer> marks = new HashMap<>();
+        HashMap<Integer, Integer> mapp = new HashMap<>();
         for(int i=0;i<arr.length;i++){
-            
+            mapp.put(arr[i],mapp.getOrDefault(arr[i], 0)+1);
         }
-    }
+        System.out.println(mapp);
+}
 }
