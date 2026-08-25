f06564f88f890d5ba9185610c93b84bd42b30f3f
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-25 21:42:46 +0530

    first commit

--- task72.java
+++ task72.java
@@ -1,18 +1,21 @@
 import java.util.HashMap;
 public class task72 {
     public static void main(String[] args) {
-        int[] arr={8,3,6,8,7};
-         HashMap<Integer, Integer> map = new HashMap<>();
-         int target=10;
-         for(int i=0;i<arr.length;i++){
-            map.put(arr[i],i);
-            int needed=target-arr[i];
-            if(map.containsKey(needed)){
-                System.out.println(map.get(arr[i]));
-                System.out.println(map.get(needed));
-                return;
-            }
-         }
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
 
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
+        return new int[] { -1, -1 };
+    }
 }
