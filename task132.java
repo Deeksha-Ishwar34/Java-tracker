ea4aeff54a4c05dae06cee52efca94461aa786d5
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-26 09:50:35 +0530

    first commit

--- task132.java
+++ task132.java
@@ -1,3 +1,19 @@
 public class task132 {
-    
+    public static void main(String[] args) {
+         int[] arr={1,2,3,4,5,6,7};
+         int k=7;
+        rotate(arr,k);
+    }
+     public static void rotate(int[] arr,int k){
+       int[] ans=new int[arr.length];
+       int len=0;
+       for(int i=arr.length-k;i<arr.length;i++){
+        ans[len]=arr[i];
+        len++;
+       }
+        for(int j=0;j<k;j++){
+         ans[len]=arr[j];
+       }
+       
+     }
 }
