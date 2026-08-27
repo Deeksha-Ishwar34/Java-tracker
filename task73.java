a000f831361ce4f34e4f66f66c85d71589451032
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-27 21:27:07 +0530

    first commit

--- task73.java
+++ task73.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class task73 {
     public static void main(String[] args) {
-        int[] arr={0,0,1,1,1,2,2,3,3,4};
+        int[] arr={1,1,2};
         removeduplicate(arr);
     }
     public static void removeduplicate(int[] arr){
@@ -12,6 +12,7 @@ public class task73 {
                 arr[slow]=arr[fast];
             }
         }
+        System.out.println(slow+1);
         System.out.println(Arrays.toString(arr));
         
     }
