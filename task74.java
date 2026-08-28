ae79036ad98d5b491ef933974f406f23a3afaeed
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-28 22:03:03 +0530

    first commit

--- task74.java
+++ task74.java
@@ -3,6 +3,8 @@ public class task74 {
         String s = "deeksha";
         System.out.println(s);
         StringBuilder sb = new StringBuilder(s);
+        char[] s1=s.toCharArray();
+        System.out.println(s1[0]);
         sb.setCharAt(0, 'r');
         sb.setCharAt(1, 'i');
         System.out.println(sb);
