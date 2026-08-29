d8448c8f45a2374a8c132c4c9752e91382e636fd
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-29 21:36:36 +0530

    first commit

--- task75.java
+++ task75.java
@@ -1,6 +1,6 @@
 public class task75 {
     public static void main(String[] args) {
-        String s=" Hello world ";
+        String s="Hello world ";
         System.out.println(s);
         System.out.println(s.trim());
         System.out.println(s.length());
