dda559a0e080975ec8e65c404da3e68cc7710a3c
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-08-30 15:45:42 +0530

    first commit

--- task133.java
+++ task133.java
@@ -1,3 +1,17 @@
+class Circle{
+        double radius;
+        void area(){
+             System.out.printf("Area:-%.2f\n",radius*radius*3.14);
+        }
+        void circum(){
+            System.out.printf("Circum:-%.2f\n",2*3.14*radius);
+        }
+    }
 public class task133 {
-    
+    public static void main(String[] args) {
+        Circle c=new Circle();
+        c.radius=4;
+        c.area();
+        c.circum();
+    }
 }
