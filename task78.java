eda5a7c10f9cf1e881a429934d104321a0d67b37
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-01 21:56:48 +0530

    first commit

--- task78.java
+++ task78.java
@@ -1,5 +1,26 @@
 public class task78 {
     public static void main(String[] args) {
         int[] arr={1, 2, 4, 6, 8};
+        int target=10;
+        boolean result=check(arr,target);
+        System.out.println(result);
     }
+    public static boolean check(int[] arr,int target){
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+            int sum=arr[left]+arr[right];
+            if(sum==target){
+                return true;
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }  
+        }
+         return false;
+    }
+   
 }
