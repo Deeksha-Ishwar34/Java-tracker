904f0209e95f14fe6c595d92b83a0a9987341c4c
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-02 22:01:06 +0530

    first commit

--- task77.java
+++ task77.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class task77 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a string:- ");
+        System.out.println("Enter a string:- ");
         String s = sc.nextLine();
         reverseCheck(s);
         sc.close();
