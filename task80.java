0a5ada92b381cf5662ee5c298c4d179a55f59606
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-07 22:10:09 +0530

    first commit

--- task80.java
+++ task80.java
@@ -1,5 +1,29 @@
 public class task80 {
     public static void main(String[] args) {
-        
+        int[] arr = { 1, 1, 2, 3, 4 };
+        int target = 5;
+        uni(arr, target);
+    }
+    public static void uni(int[] arr,int target){
+    int left = 0;
+    int right = arr.length - 1;
+    while(left < right){
+        int sum=arr[left]+arr[right];
+        if(sum==target){
+            System.out.printf("(%d %d)",arr[left],arr[right]);
+            left++;
+            right--;
+        }
+        else if(sum<target ){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+
+
+
+
     }
 }
