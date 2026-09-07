c350676e97deb44b06d87c8cf1c65a844a74b19e
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-07 22:10:42 +0530

    first commit

--- task80.java
+++ task80.java
@@ -1,6 +1,6 @@
 public class task80 {
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 2, 3, 4 };
+        int[] arr = {1, 1, 2, 3, 4 };
         int target = 5;
         uni(arr, target);
     }
