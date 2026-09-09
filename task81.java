f4ae37bcf4a1f161bb45956a60625daabc51a47d
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-09 21:40:02 +0530

    first commit

--- task81.java
+++ task81.java
@@ -1,3 +1,25 @@
 public class task81 {
-    
+    public static void main(String[] args) {
+        int[] arr={2, 7, 11, 15};
+        int target=9;
+        twoSum(arr,target);
+    }
+    public static void twoSum(int[] arr,int target){
+        int l=0;
+        int r=arr.length-1;
+        while(l<r){
+            int sum=arr[l]+arr[r];
+            if(sum==target){
+                System.out.printf("[%d,%d]",l+1,r+1);
+                break;
+            }
+            else if(sum<target){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        
+    }
 }
