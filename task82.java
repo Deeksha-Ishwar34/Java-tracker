0276aad304cdc2d6fed0dacede7de0ad9d65c12a
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-10 20:25:36 +0530

    first commit

--- task82.java
+++ task82.java
@@ -1,16 +1,17 @@
 public class task82 {
     public static void main(String[] args) {
         int[] arr1={-4, -1, 0, 3, 10};
-        int[] arr2={3, -1, 0, 3, 10};
+        int[] arr2={-4, -1, 0, 3, 10};
         check(arr1,arr2);
     }
     public static void check(int[] arr1,int[] arr2){
-       
-        for(int i=0;i<arr1.length;i++){
-             if(arr1[i]!=arr2[i]){
+       int l=0;
+        for(int r=0;r<arr1.length;r++){
+             if(arr1[l]!=arr2[r]){
             System.out.println("Not equal");
             return;
         }
+        l++;
         }
        System.out.println("Equal");
     }
