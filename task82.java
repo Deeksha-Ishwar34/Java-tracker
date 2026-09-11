71a77bfee93b365fb920a0755431245c4eb6f556
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-11 22:29:50 +0530

    first commit

--- task82.java
+++ task82.java
@@ -1,7 +1,7 @@
 public class task82 {
     public static void main(String[] args) {
         int[] arr1={-4,-1,0,3,10};
-        int[] arr2={-4,-1, 0, 3, 10};
+        int[] arr2={-4,-1,0, 3, 10};
         check(arr1,arr2);
     }
     public static void check(int[] arr1,int[] arr2){
