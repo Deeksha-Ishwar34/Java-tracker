b7a802d91456cf0cd199b91ce04af55ee4542923
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-12 21:56:42 +0530

    first commit

--- task83.java
+++ task83.java
@@ -1,5 +1,20 @@
 public class task83 {
     public static void main(String[] args) {
-        
+        String a="madam";
+        System.out.println(CheckPalindrome(a));
+    }
+    public static boolean CheckPalindrome(String a){
+        a=a.toLowerCase();
+        char[] str=a.toCharArray();
+        int left=0;
+        int right=str.length-1;
+        while(left<right){
+            if(str[left]!=str[right]){
+                return false;
+            }
+             left++;
+                right--;
+        }
+        return true;
     }
 }
