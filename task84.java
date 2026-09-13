9426909e7fff0c038a0578ebb569437cb8251f5a
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-13 22:00:07 +0530

    first commit

--- task84.java
+++ task84.java
@@ -1,5 +1,21 @@
 public class task84 {
     public static void main(String[] args) {
-        
+        int[] arr={1, 2, 3, 4, 5};
+        reverse(arr);
+    }
+    public static void reverse(int[] arr){
+        int l=0;
+        int r=arr.length-1;
+        int temp;
+        while(l<r){
+            temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
+        }
+     for(int x:arr){
+        System.out.println(x);
+     }
     }
 }
