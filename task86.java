56d6900bd2a99e72a2b8fb71dc1283c34ae4cfd6
Deeksha Ishwar <deeksha.ishwar34@gmail.com>
2026-09-17 21:40:21 +0530

    first commit

--- task86.java
+++ task86.java
@@ -1,27 +1,30 @@
 public class task86 {
     public static void main(String[] args) {
-        int[] arr={1, 2, 3, 4, 5};
-        int target=9;
-        pair(arr,target);
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 9;
+        pair(arr, target);
     }
-    public static void pair(int[] arr,int target){
-        int l=0;
-        int r=arr.length-1;
-        for(int m=1;m<arr.length;m++){
-            int sum=arr[l]+arr[r]+arr[m];
-            if(sum==target){
-                System.out.printf("%d,%d,%d",arr[l],arr[m],arr[r]);
-                l++;
-                r--;
+
+    public static void pair(int[] arr, int target) {
+        for (int l = 0; l < arr.length - 2; l++) {
+            int m = l + 1;
+            int r = arr.length - 1;
+
+            while (m < r) {
+                int sum = arr[l] + arr[m] + arr[r];
+
+                if (sum == target) {
+                    System.out.printf("%d,%d,%d\n", arr[l], arr[m], arr[r]);
+                    m++;
+                    r--;
+                } 
+                else if (sum > target) {
+                    r--;
+                } 
+                else {
+                    m++;
+                }
             }
-            else if(sum>target){
-                r--;
-            }
-            else{
-                l++;
-                r--;
-            }
-            
         }
     }
 }
